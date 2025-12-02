f9c76ad5aee1cc5a2ca63d5f2d754e84f31bfe2b
Kunal Singh <kunaldsingh26@gmail.com>
2025-12-01 20:29:11 -0800

    Project Creation

--- server/server.js
+++ server/server.js
@@ -36,7 +36,8 @@ const ENV_VARIABLES = [
 ];
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '20mb' }));
+app.use(express.urlencoded({ extended: true, limit: '20mb' }));
 fs.mkdirSync(UPLOAD_DIR, { recursive: true });
 app.use('/uploads', express.static(UPLOAD_DIR));
 
